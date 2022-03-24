5d308f0de4d02ec8ecbe0d50be43b4e5a0e61bbd
P K Agrawal <pranshukumar1999@gmail.com>
2022-03-24 12:55:16 +0530

    Airdrop project Done

--- index.js
+++ index.js
@@ -3,7 +3,37 @@ const {
   PublicKey,
   clusterApiUrl,
   Keypair,
-  LAMPORTS_PER_SOL
+  LAMPORTS_PER_SOL,
 } = require("@solana/web3.js")
 
 const wallet = new Keypair()
+
+const publicKey = new PublicKey(wallet._keypair.publicKey)
+
+const getWalletbalance = async() => {
+  try {
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+    const walletBalance = await connection.getBalance(publicKey)
+    console.log(`Wallet Balance is:${walletBalance}`)
+  } catch (error) {
+    console.log(error)
+  }
+}
+const airDropSol = async() =>{
+  try {
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+    const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
+    await connection.confirmTransaction(fromAirDropSignature)
+
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const main = async() => {
+  await getWalletbalance()
+  await airDropSol()
+  await getWalletbalance()
+}
+main()
